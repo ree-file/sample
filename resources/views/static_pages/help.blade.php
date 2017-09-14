4949db940f8655d8fceb6724a694d1df48367af2
ree_file <1205730728@qq.com>
2017-09-14 06:54:10 +0000

    Finish static pages

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+    @extends('layouts.default')
+    @section('content')
+    @section('title',"Sample帮助页")
+    <h1>帮助页</h1>
+    @stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+  @extends('layouts.default')
+  @section('title',"Sample主页")
+  @section('content')
+  <h1>主页2</h1>
+  @stop
