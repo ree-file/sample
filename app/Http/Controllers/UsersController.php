5faa2f8c5d4641d3066910fc06427061a2c4f9a4
ree_file <1205730728@qq.com>
2017-09-20 14:31:02 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 class UsersController extends Controller
 {
     //
@@ -12,24 +13,22 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
-    public function show(User $user)
-    {
+    public function show(User $user){
         return view('users.show',compact('user'));
     }
-    public function store(Request $request)
-    {
-            $this->validate($request,[
-                'name' => 'required|max:50',
-                'email' => 'required|email|unique:users|max:255',
-                'password' => 'required|confirmed',
-                'password_confirmation' => 'required'
-            ]);
-            $user = User::create([
-                'name' => $request->name,
-                'email'=>$request->email,
-                'password' => bcrypt($request->password),
-            ]);
-            session()->flash('success',"欢迎，您将在这开启一段新旅程");
-            return redirect()->route('users.show',[$user]);
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        Auth::login($user);
+        session()->flash('success',"欢迎，您将在这开启一段旅程!");
+        return redirect()->route('users.show',[$user]);
     }
 }
